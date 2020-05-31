be33f8a87b650cdc5fc525c2f5a50c96536b276c
Asya Troyan <asya99@yandex.ru>
2020-05-31 15:01:39 +0500

    create library

--- AlgoBentleyOttman/Program.cs
+++ AlgoBentleyOttman/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using BentleyOttman;
 
 namespace AlgoBentleyOttman
 {
@@ -9,75 +10,23 @@ namespace AlgoBentleyOttman
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Start program: "+ DateTime.Now.Minute+":" + DateTime.Now.Second+ ":"+DateTime.Now.Millisecond);
+
             RepairRule rule1 = new RepairRule(new DateTime(2020,6,1, 9,0,0), new DateTime(2020,6,1,17,0,0), 1, "день");
             RepairRule rule2 = new RepairRule(new DateTime(2020,6,1, 6,0,0), new DateTime(2020,6,1,7,0,0), 1, "день");
             RepairExclusion exclusion1 = new RepairExclusion(new DateTime(2020,6,6, 0,0,0), new DateTime(2020,6,8,0,0,0), 7, "день");
             RepairExclusion exclusion2 = new RepairExclusion(new DateTime(2020,6,2, 13,0,0), new DateTime(2020,6,2,14,0,0), 7, "день");
 
-            List<Tuple<DateTime,DateTime>> result = new List<Tuple<DateTime, DateTime>>();
-
-            Dictionary<DateTime, MainDicStructure> MainDictionary = new Dictionary<DateTime, MainDicStructure>();
-
-            MainDictionary.AddInterval(rule1);
-            MainDictionary.AddInterval(rule2);
-            MainDictionary.AddInterval(exclusion1);
-            MainDictionary.AddInterval(exclusion2);
-
-            bool isExclusionInAction = false;
-            bool isRuleInAction = false;
-            Tuple <DateTime,DateTime > resultCandidate = null;
-
-            foreach (KeyValuePair<DateTime, MainDicStructure> timeEvent in MainDictionary.OrderBy(x => x.Key))
-            {
-                if (timeEvent.Value.RuleExclusion == false) //This is exclusion
-                {
-                    if (timeEvent.Value.IsOpen == true) // open
-                    {
-                        isExclusionInAction = true;
-
-                        if (isRuleInAction)
-                        {
-                            resultCandidate = new Tuple<DateTime, DateTime>(resultCandidate.Item1, timeEvent.Key);
-                            result.Add(resultCandidate);
-                            resultCandidate = null;
-                        }
-                    }
-                    else //close
-                    {
-                        isExclusionInAction = false;
-                        if (isRuleInAction)
-                        {
-                            resultCandidate = new Tuple<DateTime, DateTime>(timeEvent.Key, new DateTime(0));
-                        }
-                    }
-
-                }
-                else if (timeEvent.Value.RuleExclusion == true) //This is rule
-                {
-                    if (timeEvent.Value.IsOpen == true) //open
-                    {
-                        isRuleInAction = true;
-                        resultCandidate = new Tuple<DateTime, DateTime>(timeEvent.Key, new DateTime(0));
-                    }
-                    else //close
-                    {
-                        isRuleInAction = false;
-                        if (isExclusionInAction)
-                        {
-                            resultCandidate = null;
-                        }
-                        else
-                        {
-                            resultCandidate = new Tuple<DateTime, DateTime>(resultCandidate.Item1, timeEvent.Key);
-                            result.Add(resultCandidate);
-                            resultCandidate = null;
-                        }
-                    }
-                }
-            }
-            
-
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Before loop: " + DateTime.Now.Minute + ":" + DateTime.Now.Second + ":" + DateTime.Now.Millisecond);
+            BentleyOttmanAlgorithm algo = new BentleyOttmanAlgorithm();
+             algo.AddRule(rule1);
+             algo.AddRule(rule2);
+             algo.AddRule(exclusion1);
+             algo.AddRule(exclusion2);
+             algo.GetResult();
+             
+            Console.WriteLine("After loop: " + DateTime.Now.Minute + ":" + DateTime.Now.Second + ":" + DateTime.Now.Millisecond);
+            Console.WriteLine("Finish!");
         }
     }
 }
---
+++ BentleyOttman/BentleyOttmanAlgorithm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgoBentleyOttman;
+
+namespace BentleyOttman
+{
+    public class BentleyOttmanAlgorithm
+    {
+        Dictionary<DateTime, MainDicStructure> MainDictionary = new Dictionary<DateTime, MainDicStructure>();
+        List<Tuple<DateTime, DateTime>> result = new List<Tuple<DateTime, DateTime>>();
+
+        public void AddRule(IBaseRule rule)
+        {
+            MainDictionary.AddInterval(rule);
+        }
+
+        public List<Tuple<DateTime, DateTime>> GetResult()
+        {
+            bool isExclusionInAction = false;
+            bool isRuleInAction = false;
+            Tuple<DateTime, DateTime> resultCandidate = null;
+
+            foreach (KeyValuePair<DateTime, MainDicStructure> timeEvent in MainDictionary.OrderBy(x => x.Key))
+            {
+                if (timeEvent.Value.RuleExclusion == false) //This is exclusion
+                {
+                    if (timeEvent.Value.IsOpen == true) // open
+                    {
+                        isExclusionInAction = true;
+
+                        if (isRuleInAction)
+                        {
+                            resultCandidate = new Tuple<DateTime, DateTime>(resultCandidate.Item1, timeEvent.Key);
+                            result.Add(resultCandidate);
+                            resultCandidate = null;
+                        }
+                    }
+                    else //close
+                    {
+                        isExclusionInAction = false;
+                        if (isRuleInAction)
+                        {
+                            resultCandidate = new Tuple<DateTime, DateTime>(timeEvent.Key, new DateTime(0));
+                        }
+                    }
+
+                }
+                else if (timeEvent.Value.RuleExclusion == true) //This is rule
+                {
+                    if (timeEvent.Value.IsOpen == true) //open
+                    {
+                        isRuleInAction = true;
+                        resultCandidate = new Tuple<DateTime, DateTime>(timeEvent.Key, new DateTime(0));
+                    }
+                    else //close
+                    {
+                        isRuleInAction = false;
+                        if (isExclusionInAction)
+                        {
+                            resultCandidate = null;
+                        }
+                        else
+                        {
+                            resultCandidate = new Tuple<DateTime, DateTime>(resultCandidate.Item1, timeEvent.Key);
+                            result.Add(resultCandidate);
+                            resultCandidate = null;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
